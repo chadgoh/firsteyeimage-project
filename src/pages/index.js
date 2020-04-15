8b7caf355fbf344271d39d124c2ccc79962f3634
Chad Goh <49085462+CGJL@users.noreply.github.com>
2020-04-15 17:51:08 +0800

    css, info, backgroundhero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import React from "react"
 import Layout from "../components/layout"
-
 import SEO from "../components/seo"
+import { graphql, Link } from "gatsby"
+import BackgroundHero from "../components/BackgroundHero"
+import Info from "../components/Info"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site .</p>
+    <BackgroundHero
+      img={data.img.childImageSharp.fluid}
+      title="First Eye Image"
+      styleClass="default-background"
+    />
+    <Info></Info>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(
+      relativePath: { eq: "julian-hochgesang-whrR1WQMdBE-unsplash.jpg" }
+    ) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage

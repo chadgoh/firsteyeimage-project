74c1de90a99a56d15fc112318f001dc8f9725742
Chad <chad@pop-os.localdomain>
2020-05-24 18:44:53 +0800

    redid image caption again

--- src/components/BackgroundHero.js
+++ src/components/BackgroundHero.js
@@ -1,10 +1,13 @@
 import React from "react"
 import BackgroundImage from "gatsby-background-image"
 
-function BackgroundHero({ img, styleClass, title, children, alt, color }) {
+function BackgroundHero({ img, styleClass, title, children, alt }) {
   return (
     <BackgroundImage className={styleClass} fluid={img} alt={alt}>
-      <h1 className="title text-white text-uppercase display-4 font-weight-bold ">
+      <h1
+        style={{ background: "black", fontSize: "4vw" }}
+        className="title text-white px-2 text-uppercase display-4 font-weight-bold "
+      >
         {title}
       </h1>
       {children}
